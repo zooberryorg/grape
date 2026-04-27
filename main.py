7bb7b1e68508bce93266415c682909ce87e95f18
Eric Galvan <lericgalvan@outlook.com>
2026-04-26 22:59:17 -0700

    Add header; row; column

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from nicegui import ui
 
-ui.label('Hello World').classes('text-2xl text-bold')
+with ui.header().classes('bg-gray-800 text-white'):
+    ui.label('GrAPE').classes('text-2xl font-bold')
+
+with ui.row().classes('flex-nowrap h-screen flex flex-row'):
+
+    with ui.column().classes('p-4 max-w-[80px] bg-gray-100'):
+        ui.button(icon='dashboard').classes('w-full')
+        ui.button(icon='settings').classes('w-full')
+        ui.button(icon='info').classes('w-full')
+
+    with ui.card().classes('w-full'):
+        ui.label('GrAPE').classes('text-2xl font-bold')
 
 ui.run(native=True)
