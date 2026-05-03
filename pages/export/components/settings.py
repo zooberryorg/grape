283d519916432d41768afde5904e88569bb71e44
Eric Galvan <lericgalvan@outlook.com>
2026-05-03 08:54:58 -0700

    Fix color picker visual bug

--- pages/export/components/settings.py
+++ pages/export/components/settings.py
@@ -10,6 +10,19 @@ from pages.export.events import (
     update_interlace,
 )
 
+def color_picker(is_disabled, disable_cls):
+    ui.label("Pick background color").classes(
+        f"text-gray-400 text-sm {disable_cls}"
+    ).props("dense size=sm")
+    with (
+        ui.button(icon="colorize")
+        .props(f"dense size=sm {'disable' if is_disabled else ''}")
+        .classes(disable_cls) as button
+    ):
+        ui.color_picker(
+            on_pick=lambda e: button.classes(f"!bg-[{e.color}]")
+        ).props("flat dense size=sm")
+
 
 @ui.refreshable
 def background_options():
@@ -23,41 +36,23 @@ def background_options():
     ):
         with expansion.add_slot("header"):
             with ui.row().classes("items-center w-full gap-0"):
-                check = ui.checkbox(
+                ui.checkbox(
                     value=True,
                     on_change=lambda e: [update_transparent_background(e.value), background_options.refresh()],
                 ).props("dense size=sm")
                 ui.label("Transparent Background").classes("ml-2 mr-2")
-            is_disabled = converter_state.transparent_background
-            disable_cls = (
-                "opacity-60 cursor-not-allowed pointer-events-none"
-                if is_disabled
-                else ""
-            )
 
-            with (
-                ui.card()
-                .classes(f"bg-transparent shadow-none w-full p-0 {disable_cls}")
-                .props("dense")
-            ):
-                with ui.column().classes(
-                    "bg-transparent w-full border-gray-600 rounded-sm gap-2 flex-nowrap text-sm"
-                ):
-                    with ui.row().classes(
-                        f"items-center w-full bg-transparent {disable_cls}"
-                    ):
-                        ui.label("Pick background color").classes(
-                            f"text-gray-400 text-sm {disable_cls}"
-                        ).props("dense size=sm")
-                        with (
-                            ui.button(icon="colorize")
-                            .props(f"dense size=sm {'disable' if is_disabled else ''}")
-                            .classes(disable_cls) as button
-                        ):
-                            ui.color_picker(
-                                on_pick=lambda e: button.classes(f"!bg-[{e.color}]")
-                            ).props("flat dense size=sm")
+        is_disabled = converter_state.transparent_background
+        disable_cls = (
+            "opacity-60 cursor-not-allowed pointer-events-none"
+            if is_disabled
+            else ""
+        )
 
+        with ui.row().classes(
+            f"items-center w-full bg-transparent {disable_cls}"
+        ):
+            color_picker(is_disabled, disable_cls)
 
 def export_button():
     ui.button("Export", icon="save", on_click=lambda: export_dialog()).classes(
