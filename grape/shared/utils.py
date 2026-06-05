9cc7ebb5d9cec41ff434d66ea44dc9e8770e4f3d
Eric Galvan <lericgalvan@outlook.com>
2026-06-05 12:59:39 -0700

    Add add_script

--- grape/shared/utils.py
+++ grape/shared/utils.py
@@ -6,3 +6,10 @@ def add_style(path):
         <style src="{path}"></style>
         """,
     )
+
+def add_script(path):
+    ui.add_html(
+        f"""
+        <script src="{path}"></script>
+        """,
+    )
