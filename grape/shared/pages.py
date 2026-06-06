204141e86a014049197dc6f7282c06ca950c238e
Eric Galvan <lericgalvan@outlook.com>
2026-06-06 14:26:49 -0700

    Remove function call from signature in sub_pages

--- grape/shared/pages.py
+++ grape/shared/pages.py
@@ -3,5 +3,5 @@ from grape.pages import home, export
 
 def init_pages():
     with ui.sub_pages() as sp:
-        sp.add("/", home.entry.run())
-        sp.add("/convert", export.entry.run())
+        sp.add("/", home.entry.run)
+        sp.add("/convert", export.entry.run)
