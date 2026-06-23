e4f93cd0ad75b8cbc36057a97bb063e191760b81
Eric Galvan <lericgalvan@outlook.com>
2026-06-23 02:25:46 -0700

    Remove `fabric.Object` Frame class

--- grape/static/frame.js
+++ grape/static/frame.js
@@ -1,18 +1,7 @@
-class Frame extends fabric.Object {
-    constructor(options) {
-        super(options);
-        this.type = 'frame';
-        this.order = 0;
-        this.width = 100;
-        this.height = 100;
-        this.fill = 'rgba(0,0,0,0.1)';
-        this.offsetX = 0;
-        this.offsetY = 0;
-    }
-}
+import { FabricImage, classRegistry } from 'fabric';
 
-class Layers extends fabric.Group {
-    constructor(objects, options) {
+class Frame extends FabricImage {
+    constructor(objects, options = {}) {
         super(objects, options);
         this.type = 'layers';
         this.order = 1;
