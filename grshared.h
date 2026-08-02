c9c1e485e7e06b3d30708e67fd2a7972a14447a2
Eric Galvan <lericgalvan@outlook.com>
2026-08-02 07:43:31 -0700

    Define GrShared::types

--- grshared.h
+++ grshared.h
@@ -1,4 +1,28 @@
 #ifndef GRSHARED_H
 #define GRSHARED_H
 
+#include <QVector>
+
+namespace GrShared {
+
+inline const QVector<QString> types = {
+    "ambient",
+    "animals",
+    "fences",
+    "food",
+    "freeform",
+    "fringe",
+    "guests",
+    "items",
+    "objects",
+    "paths",
+    "research",
+    "scenario",
+    "scenery",
+    "sounds",
+    "staff"
+};
+
+}
+
 #endif // GRSHARED_H
