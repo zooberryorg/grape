211b7866171177e3ae8ed3ad86f18fe288629f35
Eric Galvan <lericgalvan@outlook.com>
2026-08-02 10:26:43 -0700

    Init load and validate for grdscanner

--- grdscanner.cpp
+++ grdscanner.cpp
@@ -1,3 +1,16 @@
 #include "grdscanner.h"
 
-GrDScanner::GrDScanner() {}
+GrDScanner::GrDScanner(QDir dir) : dir(dir) {
+    validate();
+    load();
+}
+
+void GrDScanner::validate() {
+    if (!dir.exists()) {
+        // later send signal for popup error
+        QString e = "Error: directory not found: " + dir.path();
+    }
+}
+
+void GrDScanner::load() {
+}
--- grdscanner.h
+++ grdscanner.h
@@ -1,10 +1,21 @@
 #ifndef GRDSCANNER_H
 #define GRDSCANNER_H
 
+#include <QString>
+#include <QDir>
+#include <QtLogging>
+
+#include "grshared.h"
+
 class GrDScanner
 {
 public:
-    GrDScanner();
+    GrDScanner(QDir dir);
+    void validate();
+    void load();
+
+private:
+    QDir dir;
 };
 
 #endif // GRDSCANNER_H
