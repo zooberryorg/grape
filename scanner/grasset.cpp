bf422f2fee4c4267a4127d611ea255fb68d101d6
Eric Galvan <lericgalvan@outlook.com>
2026-08-08 03:12:17 -0700

    Initial Member section key saving

--- scanner/grasset.cpp
+++ scanner/grasset.cpp
@@ -5,5 +5,43 @@ GrAsset::GrAsset(QString dir)
 {}
 
 GrShared::AssetTypes GrAsset::determineTypeFromFile() {
+    CSimpleIniA ini;
+    ini.SetUnicode();
 
+    int rc = ini.LoadFile(m_cpath.toStdString().c_str());
+    if ( rc < 0 ) {
+        // error handling later
+        // Error loading ini file at path:
+    }
+
+    CSimpleIniA::TNamesDepend memberKeys;
+    ini.GetAllKeys("Member", memberKeys);
+    QStringList members;
+
+    for ( const auto& member : memberKeys ) {
+        members.append(QString(member.pItem));
+    }
+
+    if ( members.contains("animals") ) { // animals
+        // do stuff
+    } else if ( members.contains("paths") ) { // paths
+        // do stuff
+    } else if ( members.contains("structures") // buildings
+               || members.contains("shelters")
+        ) {
+        // do stuff
+    } else if ( members.contains("fence") // fences
+               || members.contains("lowfence")
+               || members.contains("zoofences")
+        ) {
+        // do stuff
+    } else if ( members.contains("scenery") // scenery
+               || members.contains("light")
+               || members.contains("rocks")
+               || members.contains("foliage")
+               || members.contains("habitatfoliage")
+               || members.contains("zoofoliage")
+        ) {
+        // do stuff
+    }
 }
