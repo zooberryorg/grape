ae7e528ec22d41cf427db801584411defe93b147
Eric Galvan <lericgalvan@outlook.com>
2026-08-10 01:16:03 -0700

    Create `GrProjectTreeModel`

---
+++ models/grprojecttreemodel.cpp
@@ -0,0 +1,3 @@
+#include "grprojecttreemodel.h"
+
+GrProjectTreeModel::GrProjectTreeModel() {}
---
+++ models/grprojecttreemodel.h
@@ -0,0 +1,11 @@
+#ifndef GRPROJECTTREEMODEL_H
+#define GRPROJECTTREEMODEL_H
+
+class GrProjectTreeModel
+{
+    Q_OBJECT
+public:
+    GrProjectTreeModel();
+};
+
+#endif // GRPROJECTTREEMODEL_H
