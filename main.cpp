ea5972422fb7cbcdb478a280bf7d698a67589102
Eric Galvan <lericgalvan@outlook.com>
2026-08-14 03:23:37 -0700

    Add msg handler to see debugs

--- main.cpp
+++ main.cpp
@@ -5,8 +5,22 @@
 #include <QTranslator>
 #include <QtWidgets>
 
+#include <QtLogging>
+#include <QCoreApplication>
+#include <QDebug>
+#include <iostream>
+
+void msgHandler( QtMsgType type, const QMessageLogContext &context, const QString &msg )
+{
+    const char symbols[] = { 'I', 'E', '!', 'X' };
+    QString output = QString("[%1] %2").arg( symbols[type] ).arg( msg );
+    std::cerr << output.toStdString() << std::endl;
+    if( type == QtFatalMsg ) abort();
+}
+
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler( msgHandler );
     QApplication a(argc, argv);
 
     QTranslator translator;
