7f49f7d67f1b4fcb8ab0d38810f74bdc3504f338
Eric Galvan <lericgalvan@outlook.com>
2026-08-14 23:43:40 -0700

    Setup fonts in application

--- ui/grwelcomescreen.cpp
+++ ui/grwelcomescreen.cpp
@@ -5,10 +5,19 @@
 
 GrWelcomeScreen::GrWelcomeScreen()
 {
+    QFont robotoHeader("Roboto");
+    robotoHeader.setPointSize(16);
+    robotoHeader.setWeight(QFont::Bold);
+
+    QFont robotoSubheader("Roboto");
+    robotoSubheader.setPointSize(12);
+    robotoSubheader.setWeight(QFont::DemiBold);
+
     workspaceHLayout = new QHBoxLayout(this);
     workspaceVLLayout = new QVBoxLayout(this);
 
-    newProjectLabel = new QLabel("New Project");
+    newProjectLabel = new QLabel("New Project", this);
+    newProjectLabel->setFont(robotoSubheader);
 
     workspaceHLayout->addLayout(workspaceVLLayout);
     workspaceVLLayout->addWidget(newProjectLabel);
