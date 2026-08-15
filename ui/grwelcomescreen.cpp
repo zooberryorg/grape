984a7e1bd3a397d96399888cadb50681d2f6f10a
Eric Galvan <lericgalvan@outlook.com>
2026-08-14 23:24:22 -0700

    Add new project label to welcome screen

--- ui/grwelcomescreen.cpp
+++ ui/grwelcomescreen.cpp
@@ -1,7 +1,15 @@
 #include "grwelcomescreen.h"
 
+#include <QHBoxLayout>
+#include <QLabel>
 
 GrWelcomeScreen::GrWelcomeScreen()
 {
+    workspaceHLayout = new QHBoxLayout(this);
+    workspaceVLLayout = new QVBoxLayout(this);
 
+    newProjectLabel = new QLabel("New Project");
+
+    workspaceHLayout->addLayout(workspaceVLLayout);
+    workspaceVLLayout->addWidget(newProjectLabel);
 }
--- ui/grwelcomescreen.h
+++ ui/grwelcomescreen.h
@@ -3,10 +3,19 @@
 
 #include <QWidget>
 
+class QHBoxLayout;
+class QVBoxLayout;
+class QLabel;
+
 class GrWelcomeScreen : public QWidget
 {
 public:
     GrWelcomeScreen();
+private:
+    QHBoxLayout* workspaceHLayout;
+    QVBoxLayout* workspaceVLLayout;
+
+    QLabel* newProjectLabel;
 };
 
 #endif // GRWELCOMESCREEN_H
