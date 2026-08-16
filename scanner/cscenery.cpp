56817898724624b8454d20f2e90916d97d233bc2
Eric Galvan <lericgalvan@outlook.com>
2026-08-15 17:25:13 -0700

    Implement initCharInts

--- scanner/cscenery.cpp
+++ scanner/cscenery.cpp
@@ -34,7 +34,32 @@ void CScenery::initMembers()
 
 void CScenery::initCharInts()
 {
-
+    m_intchars[charInts]["cHelpId"] = "";
+    m_intchars[charInts]["cNameId"] = "";
+    m_intchars[charInts]["cHabitat"] = "";
+    m_intchars[charInts]["cLocation"] = "";
+    m_intchars[charInts]["cEra"] = "";
+    m_intchars[charInts]["cMaxFoodUnits"] = "";
+    m_intchars[charInts]["cStink"] = "";
+    m_intchars[charInts]["cEstheticWeight"] = "";
+    m_intchars[charInts]["cSelectable"] = "";
+    m_intchars[charInts]["cDeletable"] = "";
+    m_intchars[charInts]["cFoliage"] = "";
+    m_intchars[charInts]["cAutoRotate"] = "";
+    m_intchars[charInts]["cLand"] = "";
+    m_intchars[charInts]["cSwims"] = ""; // not in game files
+    m_intchars[charInts]["cUnderwater"] = "";
+    m_intchars[charInts]["cSurface"] = "";
+    m_intchars[charInts]["cSubmerge"] = "";
+    m_intchars[charInts]["cOnlySwims"] = "";
+    m_intchars[charInts]["cNeedsConfirm"] = "";
+    m_intchars[charInts]["cGawkOnlyFromFront"] = "";
+    m_intchars[charInts]["cDeadOnLand"] = "";
+    m_intchars[charInts]["cDeadOnFlatWater"] = ""; // not in game files
+    m_intchars[charInts]["cDeadUnderwater"] = ""; // not in game files
+    m_intchars[charInts]["cUsesTreeRubble"] = "";
+    m_intchars[charInts]["cForcesSceneryRubble"] = "";
+    m_intchars[charInts]["cBlocksLos"] = "";
 }
 
 void CScenery::initDefaultLcid()
