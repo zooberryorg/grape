f5344ecc8dfe96d8e589077f29e38b5619a760e7
Eric Galvan <lericgalvan@outlook.com>
2026-08-16 09:46:33 -0700

    Update setSvgColor

--- common/grgfx.cpp
+++ common/grgfx.cpp
@@ -1,10 +1,28 @@
 #include "grgfx.h"
 #include <QSvgRenderer>
+#include <QColor>
+#include <qdebug.h>
+#include <QPainter>
 
-QIcon GrGfx::setSvgColor(const QString& path, int width, int height)
+QIcon GrGfx::setSvgColor(const QString& path, const QColor& color, int width, int height)
 {
+    QSvgRenderer renderer( path );
+    if ( !renderer.isValid() ) {
+        qWarning() << "Invalid SVG file:" << path;
+        return QIcon();
+    }
+
     QPixmap map = emptyMapOfSize( width, height );
-}
+
+    QPainter painter( &map );
+    painter.setRenderHint( QPainter::Antialiasing );
+    renderer.render( &painter );
+
+    painter.setCompositionMode( QPainter::CompositionMode_SourceIn );
+    painter.fillRect( map.rect(), color );
+    painter.end();
+
+    return QIcon( map );}
 
 QPixmap GrGfx::emptyMapOfSize(int width, int height)
 {
