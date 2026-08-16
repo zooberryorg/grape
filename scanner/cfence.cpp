0d603d6ac962e3477cc283a5897087d33dbce226
Eric Galvan <lericgalvan@outlook.com>
2026-08-15 21:29:48 -0700

    Implementation of initCharInts

--- scanner/cfence.cpp
+++ scanner/cfence.cpp
@@ -17,5 +17,16 @@ void CFence::initMembers()
 
 void CFence::initCharInts()
 {
-
+    m_intchars[charInts]["cStrength"] = "";
+    m_intchars[charInts]["cLife"] = "";
+    m_intchars[charInts]["cDecayedLife"] = "";
+    m_intchars[charInts]["cDecayedDelta"] = "";
+    m_intchars[charInts]["cBreakSoundAtten"] = "";
+    m_intchars[charInts]["cOpenSoundAtten"] = "";
+    m_intchars[charInts]["cSeeThrough"] = "";
+    m_intchars[charInts]["cIsJumpable"] = "";
+    m_intchars[charInts]["cIsClimbable"] = "";
+    m_intchars[charInts]["cIndestructible"] = "";
+    m_intchars[charInts]["cIsElectrified"] = "";
+    m_intchars[charInts]["cNoDrawWater"] = "";
 }
