78a14ff6835ddccae3c9e765cde3b09481f164e3
Eric Galvan <lericgalvan@outlook.com>
2026-08-15 21:06:47 -0700

    Create `CFence` class

---
+++ scanner/cfence.cpp
@@ -0,0 +1,7 @@
+#include "cfence.h"
+
+
+CFence::CFence()
+{
+
+}
---
+++ scanner/cfence.h
@@ -0,0 +1,12 @@
+#ifndef CFENCE_H
+#define CFENCE_H
+
+#include "cscenery.h"
+
+class CFence : public CScenery
+{
+public:
+    CFence();
+};
+
+#endif // CFENCE_H
