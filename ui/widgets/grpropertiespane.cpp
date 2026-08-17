2cbcb0e0ec29ff388ec064252e902a3cf119671f
Eric Galvan <lericgalvan@outlook.com>
2026-08-17 04:34:52 -0700

    Delete GrUI class

--- ui/widgets/grpropertiespane.cpp
+++ ui/widgets/grpropertiespane.cpp
@@ -6,3 +6,48 @@ GrPropertiesPane::GrPropertiesPane(QWidget *parent)
 {
 
 }
+
+void GrPropertiesPane::loadAsset(GrAsset *asset)
+{
+
+}
+
+void GrPropertiesPane::applyToAsset()
+{
+
+}
+
+QWidget* createFieldWidget(const QString& configKey, const QVariant& value, QWidget* parent)
+{
+    QWidget* field = nullptr;
+
+    switch (value.typeId()) {
+        case QMetaType::Int: {
+            QSpinBox* spin = new QSpinBox(parent);
+            spin->setRange(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+            spin->setValue(value.toInt());
+            field = spin;
+            break;
+        }
+        case QMetaType::Double: {
+            QDoubleSpinBox* spin = new QDoubleSpinBox(parent);
+            spin->setValue(value.toDouble());
+            field = spin;
+            break;
+        }
+        case QMetaType::Bool: {
+            QCheckBox* check = new QCheckBox(parent);
+            check->setChecked(value.toBool());
+            field = check;
+            break;
+        }
+        default: {
+            QLineEdit* edit = new QLineEdit(value.toString(), parent);
+            field = edit;
+            break;
+        }
+    }
+
+    field->setProperty("configKey", configKey);
+    return field;
+}
