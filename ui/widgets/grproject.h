5e230d44abdce40e5f2b087e93414ba70ef0700b
Eric Galvan <lericgalvan@outlook.com>
2026-08-18 23:40:44 -0700

    Update grproject.h

--- ui/widgets/grproject.h
+++ ui/widgets/grproject.h
@@ -3,13 +3,21 @@
 
 #include <QWidget>
 
+class GrAsset;
+class QFrame;
+class GrPropertyPanelMgr;
+
 class GrProject : public QWidget
 {
     Q_OBJECT
 public:
     explicit GrProject(QWidget *parent = nullptr);
+    void showAsset(GrAsset* asset);
 
 signals:
+private:
+    QFrame* canvasArea;
+    GrPropertyPanelMgr* panelMgr;
 
 };
 
