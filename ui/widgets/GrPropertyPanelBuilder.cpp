94143a333052651a681de7871f50c6068b63fe51
Eric Galvan <lericgalvan@outlook.com>
2026-08-18 17:49:46 -0700

    Create GrPropertyPanelBuilder class

---
+++ ui/widgets/GrPropertyPanelBuilder.cpp
@@ -0,0 +1,8 @@
+#include "grpropertypanelbuilder.h"
+
+
+GrPropertyPanelBuilder::GrPropertyPanelBuilder(QWidget *parent)
+    : QWidget{parent}
+{
+
+}
---
+++ ui/widgets/grpropertypanelbuilder.h
@@ -0,0 +1,16 @@
+#ifndef GRPROPERTYPANELBUILDER_H
+#define GRPROPERTYPANELBUILDER_H
+
+#include <QWidget>
+
+class GrPropertyPanelBuilder : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GrPropertyPanelBuilder(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // GRPROPERTYPANELBUILDER_H
