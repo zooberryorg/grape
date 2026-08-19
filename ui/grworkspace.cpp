55348905a0045d2f05cffd9497ba836d1300839a
Eric Galvan <lericgalvan@outlook.com>
2026-08-18 23:52:54 -0700

    Move property panel widget code form workspace to property panel source

--- ui/grworkspace.cpp
+++ ui/grworkspace.cpp
@@ -20,40 +20,6 @@ GrWorkspace::GrWorkspace(QWidget *parent)
     // file tree setup
     projectTree = new GrProjectTree;
 
-    // toolbar (right sidebar)
-    QToolBar* toolbar = new QToolBar;
-    toolbar->setOrientation(Qt::Vertical);
-    toolbar->setToolButtonStyle(Qt::ToolButtonIconOnly);
-    toolbar->setMaximumWidth(50);
-
-    QActionGroup* group = new QActionGroup(this);
-    group->setExclusive(true);
-
-    QAction* idTab = toolbar->addAction(
-        GrGfx::setSvgColor(":/icons/id.svg", QColor("#12834b"), 50, 50),
-        "ID Settings"
-    );
-    idTab->setCheckable(true);
-    group->addAction(idTab);
-
-    QAction* moneyTab = toolbar->addAction(
-        GrGfx::setSvgColor(":/icons/coin.svg", QColor("#12834b"), 50, 50),
-        "Finance Settings"
-    );
-    moneyTab->setCheckable(true);
-    group->addAction(moneyTab);
-
-    QWidget* toolbarFrame = new QWidget(this);
-    QHBoxLayout* toolbarLayout = new QHBoxLayout(toolbarFrame);
-    QVBoxLayout* configLayout = new QVBoxLayout();
-
-    toolbarLayout->addWidget(toolbar);
-    toolbarLayout->addLayout(configLayout);
-
-    QWidget* placeholder = new QWidget(toolbarFrame);
-    configLayout->addWidget(placeholder);
-    placeholder->setMinimumWidth(200);
-
     hSplitter->addWidget(projectTree);
     hSplitter->setSizes({250, 524, 250});
 
--- ui/widgets/grpropertypanel.cpp
+++ ui/widgets/grpropertypanel.cpp
@@ -11,6 +11,39 @@ using Widget = GrShared::WidgetType;
 GrPropertyPanel::GrPropertyPanel(QWidget *parent)
     : QWidget{parent}
 {
+    // toolbar (right sidebar)
+    QToolBar* toolbar = new QToolBar;
+    toolbar->setOrientation(Qt::Vertical);
+    toolbar->setToolButtonStyle(Qt::ToolButtonIconOnly);
+    toolbar->setMaximumWidth(50);
+
+    QActionGroup* group = new QActionGroup(this);
+    group->setExclusive(true);
+
+    QAction* idTab = toolbar->addAction(
+        GrGfx::setSvgColor(":/icons/id.svg", QColor("#12834b"), 50, 50),
+        "ID Settings"
+    );
+    idTab->setCheckable(true);
+    group->addAction(idTab);
+
+    QAction* moneyTab = toolbar->addAction(
+        GrGfx::setSvgColor(":/icons/coin.svg", QColor("#12834b"), 50, 50),
+        "Finance Settings"
+    );
+    moneyTab->setCheckable(true);
+    group->addAction(moneyTab);
+
+    QWidget* toolbarFrame = new QWidget(this);
+    QHBoxLayout* toolbarLayout = new QHBoxLayout(toolbarFrame);
+    QVBoxLayout* configLayout = new QVBoxLayout();
+
+    toolbarLayout->addWidget(toolbar);
+    toolbarLayout->addLayout(configLayout);
+
+    QWidget* placeholder = new QWidget(toolbarFrame);
+    configLayout->addWidget(placeholder);
+    placeholder->setMinimumWidth(200);
 
 }
 
