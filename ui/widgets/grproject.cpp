a70460a34df65cd0b75faccad3589a03a74287ba
Eric Galvan <lericgalvan@outlook.com>
2026-08-18 21:11:56 -0700

    Create GrProject class

---
+++ ui/widgets/grproject.cpp
@@ -0,0 +1,8 @@
+#include "grproject.h"
+
+
+GrProject::GrProject(QWidget *parent)
+    : QWidget{parent}
+{
+
+}
---
+++ ui/widgets/grproject.h
@@ -0,0 +1,16 @@
+#ifndef GRPROJECT_H
+#define GRPROJECT_H
+
+#include <QWidget>
+
+class GrProject : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GrProject(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // GRPROJECT_H
