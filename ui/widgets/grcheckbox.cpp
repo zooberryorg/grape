25602813f7851770647da9e081ac3c1ac84fd685
Eric Galvan <lericgalvan@outlook.com>
2026-08-20 09:55:50 -0700

    Add shadow effect

--- ui/widgets/grcheckbox.cpp
+++ ui/widgets/grcheckbox.cpp
@@ -4,6 +4,7 @@
 #include <QHBoxLayout>
 #include <QCheckBox>
 #include <QLabel>
+#include <QGraphicsDropShadowEffect>
 
 GrCheckBox::GrCheckBox(QWidget *parent, const QString& label, const QString& caption)
     : QWidget{parent}
@@ -13,6 +14,13 @@ GrCheckBox::GrCheckBox(QWidget *parent, const QString& label, const QString& cap
     vLayout->setSpacing(0);
     checkbox = new QCheckBox(label);
     QLabel* captionLabel = new QLabel(caption);
+
+    QGraphicsDropShadowEffect *shadowEffect = new QGraphicsDropShadowEffect(this);
+    shadowEffect->setBlurRadius(4);
+    shadowEffect->setColor(QColor(0, 0, 0, 120));
+    shadowEffect->setOffset(0, 1);
+
+    checkbox->setGraphicsEffect(shadowEffect);
 
     vLayout->addWidget(checkbox);
     vLayout->addWidget(captionLabel);
