75f9d03420914bdc73f176200c6ff48ef21f51a2
Eric Galvan <lericgalvan@outlook.com>
2026-08-21 12:08:39 -0700

    Simple table model

---
+++ models/grlangtablemodel.cpp
@@ -0,0 +1,54 @@
+#include "grlangtablemodel.h"
+
+GrLangTableModel::GrLangTableModel(QObject *parent)
+    : QAbstractTableModel{parent}
+{
+}
+
+void GrLangTableModel::setEntries(QVector<StringEntry> entries)
+{
+    beginResetModel();
+    m_entries = std::move(entries);
+    endResetModel();
+}
+
+int GrLangTableModel::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid())
+        return 0;
+    return m_entries.size();
+}
+
+int GrLangTableModel::columnCount(const QModelIndex &parent) const
+{
+    if (parent.isValid())
+        return 0;
+    return 2;
+}
+
+QVariant GrLangTableModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid() || index.row() >= m_entries.size())
+        return QVariant();
+
+    if (role == Qt::DisplayRole || role == Qt::EditRole) {
+        const StringEntry &entry = m_entries.at(index.row());
+        switch (index.column()) {
+            case 0: return entry.id;
+            case 1: return entry.value;
+        }
+    }
+    return QVariant();
+}
+
+QVariant GrLangTableModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role != Qt::DisplayRole || orientation != Qt::Horizontal)
+        return QVariant();
+
+    switch (section) {
+        case 0: return "ID";
+        case 1: return "Value";
+    }
+    return QVariant();
+}
--- models/grlangtablemodel.h
+++ models/grlangtablemodel.h
@@ -2,15 +2,25 @@
 #define GRLANGTABLEMODEL_H
 
 #include <QAbstractTableModel>
+#include "grpe.h"
+
+using StringEntry = GrPE::Entry;
 
 class GrLangTableModel : public QAbstractTableModel
 {
     Q_OBJECT
 public:
     explicit GrLangTableModel(QObject *parent = nullptr);
+
+    void setEntries(QVector<StringEntry> entries);
+
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+private:
+    QVector<StringEntry> m_entries;
 };
 
 #endif // GRLANGTABLEMODEL_H
