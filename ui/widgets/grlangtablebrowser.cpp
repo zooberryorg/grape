a4f3d748b7516e3a3ff4d4cabe7deb5ea9111ca8
Eric Galvan <lericgalvan@outlook.com>
2026-08-21 12:10:32 -0700

    Create GrLangTableBrowser widget class

---
+++ ui/widgets/grlangtablebrowser.cpp
@@ -0,0 +1,8 @@
+#include "grlangtablebrowser.h"
+
+
+GrLangTableBrowser::GrLangTableBrowser(QWidget *parent)
+    : QWidget{parent}
+{
+
+}
---
+++ ui/widgets/grlangtablebrowser.h
@@ -0,0 +1,16 @@
+#ifndef GRLANGTABLEBROWSER_H
+#define GRLANGTABLEBROWSER_H
+
+#include <QWidget>
+
+class GrLangTableBrowser : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GrLangTableBrowser(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // GRLANGTABLEBROWSER_H
