a96794dea0e566d98395e4a5225cb579c766d1cd
Eric Galvan <lericgalvan@outlook.com>
2026-08-22 02:52:33 -0700

    Initial implementation of GrFilterMenu

--- ui/widgets/grfiltermenu.cpp
+++ ui/widgets/grfiltermenu.cpp
@@ -1,8 +1,20 @@
 #include "grfiltermenu.h"
 
+#include <QVBoxLayout>
+#include <QComboBox>
+#include <QLabel>
 
-GrFilterMenu::GrFilterMenu(QWidget *parent)
-    : QWidget{parent}
+GrFilterMenu::GrFilterMenu(QWidget* parent, QStringList dllFileNames)
+    : QWidget(parent, Qt::Popup)
 {
+    setAttribute(Qt::WA_StyledBackground, true);
+    setObjectName("filterMenu");
 
+    layout = new QVBoxLayout(this);
+
+    filterDllFiles = new QComboBox(this);
+    filterDllFiles->addItems(dllFileNames);
+
+    layout->addWidget(new QLabel("Search by File:", this));
+    layout->addWidget(filterDllFiles);
 }
