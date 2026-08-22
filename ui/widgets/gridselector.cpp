b98716b892bdf6d03d461f3dc14dc329034931ea
Eric Galvan <lericgalvan@outlook.com>
2026-08-22 07:24:19 -0700

    Initial constructor implementation for widget layout

--- ui/widgets/gridselector.cpp
+++ ui/widgets/gridselector.cpp
@@ -1,8 +1,31 @@
 #include "gridselector.h"
 
+#include "grlangtablebrowser.h"
+#include <QVBoxLayout>
+#include <QLabel>
+
+#include "graltbutton.h"
 
 GrIdSelector::GrIdSelector(QWidget *parent)
     : QWidget{parent}
 {
+    setObjectName("explorerContainer");
+    setAttribute(Qt::WA_StyledBackground, true);
 
+    layout = new QVBoxLayout(this);
+    buttonLayout = new QHBoxLayout;
+    QWidget* buttonArea = new QWidget(this);
+    buttonArea->setLayout(buttonLayout);
+
+    GrLangTableBrowser* browser = new GrLangTableBrowser;
+    QLabel* label = new QLabel("ID Picker");
+    GrAltButton* cancelButton = new GrAltButton("Cancel", this);
+    GrAltButton* selectButton = new GrAltButton("Cancel", this);
+
+    layout->addWidget(label);
+    layout->addWidget(browser);
+    layout->addWidget(buttonArea);
+
+    buttonLayout->addWidget(cancelButton);
+    buttonLayout->addWidget(selectButton);
 }
--- ui/widgets/gridselector.h
+++ ui/widgets/gridselector.h
@@ -3,12 +3,17 @@
 
 #include <QWidget>
 
+class QVBoxLayout;
+class QHBoxLayout;
+
 class GrIdSelector : public QWidget
 {
     Q_OBJECT
 public:
     explicit GrIdSelector(QWidget *parent = nullptr);
-
+private:
+    QVBoxLayout* layout;
+    QHBoxLayout* buttonLayout;
 signals:
 
 };
