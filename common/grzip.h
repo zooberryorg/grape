d1b0e62d09b36c69e08adfb5cb98dbf70a3c418a
Eric Galvan <lericgalvan@outlook.com>
2026-08-23 15:00:32 -0700

    Create GrZIP prototype class

--- common/grzip.h
+++ common/grzip.h
@@ -1,11 +1,28 @@
 #ifndef GRZIP_H
 #define GRZIP_H
 
+#include <QString>
+#include <miniz.h>
 
 class GrZIP
 {
 public:
-    GrZIP();
+    explicit GrZIP(const QString& archivePath);
+    ~GrZIP();
+
+    bool isOpen() const { return m_isOpen; }
+    QString lastError() const { return m_lastError; }
+
+    QStringList fileNames() const;
+    bool extractFileToBuffer(const QString& name, QByteArray& out) const;
+    bool extractAllTo(const QString& destDir) const;
+
+private:
+    mz_zip_archive m_archive;
+    bool m_isOpen = false;
+    mutable QString m_lastError;
+
+    void setErrorFromMiniz();
 };
 
 #endif // GRZIP_H
