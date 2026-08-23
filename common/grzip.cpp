df7e7aafd59dd939f5c79e74a4ae981a5489d748
Eric Galvan <lericgalvan@outlook.com>
2026-08-23 15:06:00 -0700

    Implement GrZIP constructor

--- common/grzip.cpp
+++ common/grzip.cpp
@@ -1,7 +1,35 @@
 #include "grzip.h"
 
 
-GrZIP::GrZIP()
+GrZIP::GrZIP(const QString& archivePath)
+{
+    memset(&m_archive, 0, sizeof(m_archive));
+    m_isOpen = mz_zip_reader_init_file(&m_archive, archivePath.toUtf8().constData(), 0);
+    if (!m_isOpen)
+        setErrorFromMiniz();
+}
+
+GrZIP::~GrZIP()
+{
+
+}
+
+QStringList GrZIP::fileNames() const
+{
+
+}
+
+bool GrZIP::extractFileToBuffer(const QString &name, QByteArray &out) const
+{
+
+}
+
+bool GrZIP::extractAllTo(const QString &destDir) const
+{
+
+}
+
+void GrZIP::setErrorFromMiniz()
 {
 
 }
