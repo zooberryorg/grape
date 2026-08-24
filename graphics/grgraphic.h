c34cb45dd627c74d46be480bcc7d626c1ba1bc5e
Eric Galvan <lericgalvan@outlook.com>
2026-08-24 06:24:59 -0700

    Declare GrPalette prototype

--- graphics/grgraphic.h
+++ graphics/grgraphic.h
@@ -1,10 +1,37 @@
 #ifndef GRGRAPHIC_H
 #define GRGRAPHIC_H
 
-class GrTexture
+#include <QObject>
+
+#include "ztalib/ZtaF.h"
+
+class GrPalette;
+
+class GrGraphic : public QObject
 {
+    Q_OBJECT
 public:
-    GrTexture();
+    GrGraphic(std::shared_ptr<ZtaData> data, GrPalette* palette, QObject* parent = nullptr);
+
+    int frameCount() const { return m_indexImages.size(); }
+    QImage compositeFrame(int frameIndex) const;
+
+    void eraseAt(int frameIndex, int x, int y);
+
+    QPoint frameOffset(int frameIndex) const;
+    uint32_t speed() const { return m_data->info.speed; }
+
+private slots:
+    void handlePaletteChanged();
+
+private:
+    void decodeFrames();
+
+    std::shared_ptr<ZtaData> m_data;
+    GrPalette* m_palette;
+
+    QVector<QImage> m_indexImages;
+    QVector<QImage> m_alphaMasks;
 };
 
 #endif // GRGRAPHIC_H
