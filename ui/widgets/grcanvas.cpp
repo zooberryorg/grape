f1949e3025c20f14f782643f1bb626129360ec06
Eric Galvan <lericgalvan@outlook.com>
2026-08-25 18:23:08 -0700

    Implementation of GrCanvas::setGraphic

--- ui/widgets/grcanvas.cpp
+++ ui/widgets/grcanvas.cpp
@@ -1,21 +1,29 @@
 #include "grcanvas.h"
 
+#include "grgraphic.h"
 
 GrCanvas::GrCanvas(QWidget *parent)
     : QWidget{parent},
       m_graphic(nullptr)
 {
-
-}
-
-GrCanvas::GrCanvasView(QWidget *parent)
-{
-
+    setObjectName("canvasArea");
+    setAttribute(Qt::WA_StyledBackground, true);
 }
 
 void GrCanvas::setGraphic(GrGraphic *graphic)
 {
+    if (m_graphic)
+        disconnect(m_graphic, nullptr, this, nullptr);
 
+    m_graphic = graphic;
+    m_currentFrame = 0;
+
+    if (m_graphic) {
+        connect(m_graphic, &GrGraphic::loaded, this, &GrCanvas::handleGraphicLoaded);
+        connect(m_graphic, &GrGraphic::frameEdited, this, &GrCanvas::handleFrameEdited);
+    }
+
+    update();
 }
 
 void GrCanvas::setFrameIndex(int index)
