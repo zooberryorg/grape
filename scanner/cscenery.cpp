a1aef53bc207a1363fe88cfa5e1cbdc56128ef4b
Eric Galvan <lericgalvan@outlook.com>
2026-08-26 13:43:01 -0700

    Convert CScenery inits to SectionTemplate types

--- scanner/cscenery.cpp
+++ scanner/cscenery.cpp
@@ -57,16 +57,27 @@ void CScenery::save()
 
 void CScenery::initGlobals()
 {
-    m_globals[globals]["Type"] = { "", Group::ID, Widget::IdPicker };
+    GrINI::SectionTemplate tmpl {
+        "Global",
+        {
+            { "Type", { "", Group::ID, Widget::IdPicker } }
+        }
+    };
+    GrINI::registerSubtypes(m_globals, tmpl, subtypes());
 }
 
 void CScenery::initIcon()
 {
-    m_icon[icons]["IconSE"] = { "", Group::Graphics, Widget::LineText };
-    m_icon[icons]["IconSW"] = { "", Group::Graphics, Widget::LineText };
-    m_icon[icons]["IconNW"] = { "", Group::Graphics, Widget::LineText };
-    m_icon[icons]["IconNE"] = { "", Group::Graphics, Widget::LineText };
-
+    GrINI::SectionTemplate tmpl {
+        "Icon",
+        {
+            { "IconSE", { "", Group::Graphics, Widget::LineText } },
+            { "IconSW", { "", Group::Graphics, Widget::LineText } },
+            { "IconNW", { "", Group::Graphics, Widget::LineText } },
+            { "IconNE", { "", Group::Graphics, Widget::LineText } }
+        }
+    };
+    GrINI::registerSubtypes(m_icon, tmpl, subtypes());
 }
 
 void CScenery::initMembers()
@@ -76,53 +87,77 @@ void CScenery::initMembers()
 
 void CScenery::initCharInts()
 {
-    m_intchars[charInts]["cPurchaseCost"] = { "", Group::Commerce, Widget::Integer };
-    m_intchars[charInts]["cHelpId"] = { "", Group::ID, Widget::IdPicker };
-    m_intchars[charInts]["cNameId"] = { "", Group::ID, Widget::IdPicker };
-    m_intchars[charInts]["cHabitat"] = { "", Group::Terrain, Widget::IdPicker };
-    m_intchars[charInts]["cLocation"] = { "", Group::Terrain, Widget::IdPicker };
-    m_intchars[charInts]["cEra"] = { "", Group::Terrain, Widget::IdPicker };
-    m_intchars[charInts]["cMaxFoodUnits"] = { "", Group::Traits, Widget::Integer };
-    m_intchars[charInts]["cStink"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cEstheticWeight"] = { "", Group::Traits, Widget::Integer };
-    m_intchars[charInts]["cSelectable"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cDeletable"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cFoliage"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cAutoRotate"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cLand"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cSwims"] = { "", Group::Traits, Widget::Switch }; // not in game files
-    m_intchars[charInts]["cUnderwater"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cSurface"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cSubmerge"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cOnlySwims"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cNeedsConfirm"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cGawkOnlyFromFront"] = { "", Group::Slots, Widget::Switch };
-    m_intchars[charInts]["cDeadOnLand"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cDeadOnFlatWater"] = { "", Group::Traits, Widget::Switch }; // not in game files
-    m_intchars[charInts]["cDeadUnderwater"] = { "", Group::Traits, Widget::Switch }; // not in game files
-    m_intchars[charInts]["cUsesTreeRubble"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cForcesSceneryRubble"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cBlocksLos"] = { "", Group::Traits, Widget::Switch };
+    GrINI::SectionTemplate tmpl {
+        "Characteristics/Integers",
+        {
+            { "cPurchaseCost", { "", Group::Commerce, Widget::Integer } },
+            { "cHelpId", { "", Group::ID, Widget::IdPicker } },
+            { "cNameId", { "", Group::ID, Widget::IdPicker } },
+            { "cHabitat", { "", Group::Terrain, Widget::IdPicker } },
+            { "cLocation", { "", Group::Terrain, Widget::IdPicker } },
+            { "cEra", { "", Group::Terrain, Widget::IdPicker } },
+            { "cMaxFoodUnits", { "", Group::Traits, Widget::Integer } },
+            { "cStink", { "", Group::Traits, Widget::Switch } },
+            { "cEstheticWeight", { "", Group::Traits, Widget::Integer } },
+            { "cSelectable", { "", Group::UI, Widget::Switch } },
+            { "cDeletable", { "", Group::UI, Widget::Switch } },
+            { "cFoliage", { "", Group::Traits, Widget::Switch } },
+            { "cAutoRotate", { "", Group::UI, Widget::Switch } },
+            { "cLand", { "", Group::Traits, Widget::Switch } },
+            { "cSwims", { "", Group::Traits, Widget::Switch } }, // not in game files
+            { "cUnderwater", { "", Group::Traits, Widget::Switch } },
+            { "cSurface", { "", Group::Traits, Widget::Switch } },
+            { "cSubmerge", { "", Group::Traits, Widget::Switch } },
+            { "cOnlySwims", { "", Group::Traits, Widget::Switch } },
+            { "cNeedsConfirm", { "", Group::UI, Widget::Switch } },
+            { "cGawkOnlyFromFront", { "", Group::Slots, Widget::Switch } },
+            { "cDeadOnLand", { "", Group::Traits, Widget::Switch } },
+            { "cDeadOnFlatWater", { "", Group::Traits, Widget::Switch } }, // not in game files
+            { "cDeadUnderwater", { "", Group::Traits, Widget::Switch } }, // not in game files
+            { "cUsesTreeRubble", { "", Group::Traits, Widget::Switch } },
+            { "cForcesSceneryRubble", { "", Group::Traits, Widget::Switch } },
+            { "cBlocksLos", { "", Group::Traits, Widget::Switch } }
+        }
+    };
+
+    GrINI::registerSubtypes(m_intchars, tmpl, subtypes());
 }
 
 void CScenery::initDefaultLcid()
 {
-    m_defaultlcid[defaultLcid]["LCID"] = { "", Group::ID, Widget::LineText };
+    GrINI::SectionTemplate tmpl {
+        "DefaultLCID",
+        {
+            { "LCID", { "", Group::ID, Widget::LineText } }
+        }
+    };
+    GrINI::registerSubtypes(m_defaultlcid, tmpl, subtypes());
 }
 
 void CScenery::init1033()
 {
-    m_1033[_1033]["cGeneralInfoFileName"] = { "", Group::Graphics, Widget::LineText };
-    m_1033[_1033]["cLongHelp"] = { "", Group::ID, Widget::MultilineText };
-    m_1033[_1033]["cName"] = { "", Group::ID, Widget::LineText };
-    m_1033[_1033]["cTheString"] = { "", Group::ID, Widget::LineText };
+    GrINI::SectionTemplate tmpl {
+        "1033",
+        {
+            { "cGeneralInfoFileName", { "", Group::Graphics, Widget::LineText } },
+            { "cLongHelp", { "", Group::ID, Widget::MultilineText } },
+            { "cName", { "", Group::ID, Widget::LineText } },
+            { "cTheString", { "", Group::ID, Widget::LineText } }
+        }
+    };
+    GrINI::registerSubtypes(m_1033, tmpl, subtypes());
 }
-
 void CScenery::initAnimations()
 {
-    m_animations[animations]["idle"] = { "", Group::Graphics, Widget::LineText };
-    m_animations[animations]["IDLE"] = { "", Group::Graphics, Widget::LineText };
-    m_animations[animations]["used"] = { "", Group::Graphics, Widget::LineText };
-    m_animations[animations]["USED"] = { "", Group::Graphics, Widget::LineText };
+    GrINI::SectionTemplate tmpl {
+        "Animations",
+        {
+            { "idle", { "", Group::Graphics, Widget::LineText } },
+            { "IDLE", { "", Group::Graphics, Widget::LineText } },
+            { "used", { "", Group::Graphics, Widget::LineText } },
+            { "USED", { "", Group::Graphics, Widget::LineText } }
+        }
+    };
 
+    GrINI::registerSubtypes(m_animations, tmpl, subtypes());
 }
