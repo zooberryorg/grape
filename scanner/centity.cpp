9c3cf5825e2c728f8cfe4cbfc9b2947c2baa0a71
Eric Galvan <lericgalvan@outlook.com>
2026-08-26 13:35:49 -0700

    Convert CEntity inits to SectionTemplate types

--- scanner/centity.cpp
+++ scanner/centity.cpp
@@ -3,30 +3,37 @@
 CEntity::CEntity(QString path)
     : GrAsset(path)
 {
-    m_intchars[charInts]["cIconZoom"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cExpansionID"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cMoveable"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["walkable"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["walkableByTall"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cRubbleable"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cUseNumbersInName"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cUsesRealShadows"] = { "", Group::Graphics, Widget::Switch };
-    m_intchars[charInts]["cHasShadowImages"] = { "", Group::Graphics, Widget::Switch };
-    m_intchars[charInts]["cForceShadowBlack"] = { "", Group::Graphics, Widget::Switch };
-    m_intchars[charInts]["cDrawsLate"] = { "", Group::Graphics, Widget::Switch };
-    m_intchars[charInts]["cHeight"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cDepth"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cHasUnderwaterSection"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cIsTransient"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cUsesPlacementCube"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cShow"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cHitThreshold"] = { "", Group::Traits, Widget::Switch };
-    m_intchars[charInts]["cAvoidEdges"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cFootprintX"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cFootprintY"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cFootprintZ"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cPlacementFootprintX"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cPlacementFootprintY"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["cPlacementFootprintZ"] = { "", Group::UI, Widget::Switch };
-    m_intchars[charInts]["AvailableAtStartup"] = { "", Group::UI, Widget::Switch };
+    GrINI::SectionTemplate tmpl {
+        "Characteristics/Integers",
+        {
+            { "cIconZoom", { "", Group::UI, Widget::Switch } },
+            { "cExpansionID", { "", Group::UI, Widget::Switch } },
+            { "cMoveable", { "", Group::UI, Widget::Switch } },
+            { "walkable", { "", Group::Traits, Widget::Switch } },
+            { "walkableByTall", { "", Group::Traits, Widget::Switch } },
+            { "cRubbleable", { "", Group::Traits, Widget::Switch } },
+            { "cUseNumbersInName", { "", Group::UI, Widget::Switch } },
+            { "cUsesRealShadows", { "", Group::Graphics, Widget::Switch } },
+            { "cHasShadowImages", { "", Group::Graphics, Widget::Switch } },
+            { "cForceShadowBlack", { "", Group::Graphics, Widget::Switch } },
+            { "cDrawsLate", { "", Group::Graphics, Widget::Switch } },
+            { "cHeight", { "", Group::Traits, Widget::Switch } },
+            { "cDepth", { "", Group::Traits, Widget::Switch } },
+            { "cHasUnderwaterSection", { "", Group::Traits, Widget::Switch } },
+            { "cIsTransient", { "", Group::Traits, Widget::Switch } },
+            { "cUsesPlacementCube", { "", Group::UI, Widget::Switch } },
+            { "cShow", { "", Group::UI, Widget::Switch } },
+            { "cHitThreshold", { "", Group::Traits, Widget::Switch } },
+            { "cAvoidEdges", { "", Group::UI, Widget::Switch } },
+            { "cFootprintX", { "", Group::UI, Widget::Switch } },
+            { "cFootprintY", { "", Group::UI, Widget::Switch } },
+            { "cFootprintZ", { "", Group::UI, Widget::Switch } },
+            { "cPlacementFootprintX", { "", Group::UI, Widget::Switch } },
+            { "cPlacementFootprintY", { "", Group::UI, Widget::Switch } },
+            { "cPlacementFootprintZ", { "", Group::UI, Widget::Switch } },
+            { "AvailableAtStartup", { "", Group::UI, Widget::Switch } }
+        }
+    };
+
+    GrINI::registeSubtypes(m_intchars, tmpl, subtypes());
 }
